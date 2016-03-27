365497bbb35f3d83f40df9a80b378c638275b86d
netcasper <netcasper@126.com>
2016-03-27 21:36:26 +0800

    Initial commit

--- app/src/main/java/com/example/wangwilliam/floatengine/ChartType.java
+++ app/src/main/java/com/example/wangwilliam/floatengine/ChartType.java
@@ -4,11 +4,10 @@ package com.example.wangwilliam.floatengine;
  * Created by wangwilliam on 3/13/16.
  */
 
-<<<<<<< HEAD
+
 // This is a test for GitHub.
 
-=======
->>>>>>> github/master
+
 public enum ChartType {
     MINUTE,
     DAY90,
